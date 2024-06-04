0f4176bd191383b0a442c402a7fc5f02ea8b9d9e
timur.abdullin <timur.abdullin@factory5.online>
2024-06-04 13:31:07 +0300

    add timer, finish the project

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,11 +1,23 @@
-export default function FinishScreen({ points, maxPossiblePoints, highScore }) {
+export default function FinishScreen({
+  points,
+  maxPossiblePoints,
+  highScore,
+  dispatch,
+}) {
   const percentage = (points / maxPossiblePoints) * 100;
   return (
     <>
       <p className="result">
         Your result is {points} / {maxPossiblePoints} - {Math.ceil(percentage)}%
       </p>
-      <p>(HighScore: {highScore} points)</p>
+      <p className="highscore">(HighScore: {highScore} points)</p>
+
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart quiz
+      </button>
     </>
   );
 }
